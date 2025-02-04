03d4cb1dd5f4482f735ddf4ae559ee963d002b77
Max Kuznetsov <kuzma-2000@mail.riu>
2025-02-04 22:22:14 +0300

    initial

---
+++ ESP32/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ESP32
+//
+//  Created by Max on 04.02.2025.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
